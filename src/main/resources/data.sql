bac843ddbf3518f13ef31017513e0449476cf014
choijungwoo493 <jwoo1016@gmail.com>
2022-11-30 18:11:31 +0900

    make catalog-service microservice

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into catalog(product_id,product_name,stock,unit_price)
+    values('CATALOG-001','BERLIN',100,1500);
+insert into catalog(product_id,product_name,stock,unit_price)
+    values('CATALOG-002','CHINA',110,15);
+insert into catalog(product_id,product_name,stock,unit_price)
+    values('CATALOG-003','KOREA',120,15000)
